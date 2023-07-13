baf4681cf424453ebbd0a958e03580200a528526
Prince <princegujjar1.pp@gmail.com>
2023-07-13 18:41:25 +0530

    smart contract

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,10 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-//import "hardhat/console.sol";
-
-contract Assessment {
-    address payable public owner;
+// SPDX-License-Identifier: MIT
+ pragma solidity ^0.8.9;
+ 
+ contract Atm{
+    address payable owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
@@ -12,49 +10,19 @@ contract Assessment {
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
-        balance = initBalance;
+        balance=initBalance;
     }
-
     function getBalance() public view returns(uint256){
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
+    function deposit(uint256 _amount) public payable{
         uint _previousBalance = balance;
+        require(msg.sender==owner,"Not Authorized");
 
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
+        balance+=_amount;
 
-        // perform transaction
-        balance += _amount;
+        assert(balance==_previousBalance+_amount);
 
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
     }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
-    }
-}
+ }
